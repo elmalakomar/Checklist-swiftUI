c2f35362f1513dd95c11a0ea10489a74bcdc1ecb
Omar <elmalakomar@gmail.com>
2020-09-23 12:22:15 +0200

    Initial Commit

---
+++ Checklists/ChecklistsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChecklistsApp.swift
+//  Checklists
+//
+//  Created by Omar El Malak on 23/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct ChecklistsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
